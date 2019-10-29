7243fbd261605f13b86d1d7b03e79609c479d689
root <root@gitclient.us-central1-a.c.hypnotic-surge-251812.internal>
2019-10-29 12:18:59 +0000

    First commit

---
+++ python.py
@@ -0,0 +1,14 @@
+brand=input('Enter your favorate Brand:')
+if brand=='KF':
+	print('It is childerans brand')
+elif brand=='RC':
+	print('It is not that much kick')
+elif brand == 'KO':
+	print ('It is too light')
+elif brand=='FO':
+	print ('Buy one get one free')
+elif brand=='Budwiser':
+	print('without Budwiser my lif is wase')
+else:
+	print('Other brands are not recomented')
+
